c1dc6aa36711637e665bad8898755e8118ea82ea
mvllow <mvllow@icloud.com>
2021-06-15 10:41:55 -0500

    init

---
+++ source/index.ts
@@ -0,0 +1,10 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+function lib() {
+	console.log(`${__dirname}\nCiao bella`)
+}
+
+export { lib }
