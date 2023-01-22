4d40ee9de34e33218c2cda335b6dbe7e701d27ac
mvllow <mvllow@icloud.com>
2023-01-21 22:23:45 -0500

    feat!: drop Node.js 12 support

--- test/main.ts
+++ test/main.ts
@@ -1,8 +1,8 @@
-import test from 'ava'
-import add from '../source/index.js'
+import test from 'ava';
+import add from '../source/index.js';
 
 test('main', (t) => {
-	t.is(add(0, 0), 0)
-	t.is(add(41, 68), 109)
-	t.is(add(-10, 20), 10)
-})
+	t.is(add(0, 0), 0);
+	t.is(add(41, 68), 109);
+	t.is(add(-10, 20), 10);
+});
